5530e8458458df18af682b874f24591066159ba1
Rohini4258 <rohini4258@gmail.com>
2024-07-31 13:31:55 -0400

    new file

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "ami_id" {
+  type        = string
+  default     = ""
+  description = "Pass value to main.tf"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = ""
+  description = "Pass value to main.tf"
+}
+
+variable "key" {
+  type        = string
+  default     = ""
+  description = "pass the value to main"
+}
